d2718858800903586f425f9e8d7b294064db5184
Madhura <madhura.bhujbal@gmail.com>
2020-03-16 16:46:40 +0000

    Split string to words

--- node-core-examples/main.js
+++ node-core-examples/main.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
 
 var file = fs.readFileSync('mini-input.dat', 'utf-8');
-console.log(file);
+//console.log(file);
+var splitWords = file.split('|');
+console.log(splitWords);
+//var sortedFile = fs.writeFileSync('sortedfile.dat', 'sort');
 
