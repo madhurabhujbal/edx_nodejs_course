70c13181126adff17088ddd8d7028f11f5c99cbd
Madhura <madhura.bhujbal@gmail.com>
2020-03-16 17:12:49 +0000

    Convert first alphabet to uppercase

--- node-core-examples/main.js
+++ node-core-examples/main.js
@@ -3,6 +3,10 @@ var fs = require('fs');
 var file = fs.readFileSync('mini-input.dat', 'utf-8');
 //console.log(file);
 var splitWords = file.split('|');
-console.log(splitWords);
-//var sortedFile = fs.writeFileSync('sortedfile.dat', 'sort');
+//console.log(splitWords);
+
+var firstWord = splitWords[0];
+//console.log(firstWord);
+splitfw = firstWord[0].toUpperCase() + firstWord.slice(1);
+console.log(splitfw);
 
