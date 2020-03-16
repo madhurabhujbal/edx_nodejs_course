f305ae5370b219a251d9b55d20f3b405592adf9f
Madhura <madhura.bhujbal@gmail.com>
2020-03-16 22:45:51 +0000

    Invoke function returning string length

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const modFile = require('./module');
 
 console.log(`Welcome message : ${modFile.exhello()}`);
+
+let name = 'Madhura Bhujbal';
+let nameLength = modFile.stringLength(name);
+console.log('Message length :' + nameLength);
