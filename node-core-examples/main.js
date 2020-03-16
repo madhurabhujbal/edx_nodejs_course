3c1aca7ccc04d6a66e25651b457e1805feff7c1a
Madhura <madhura.bhujbal@gmail.com>
2020-03-16 18:00:32 +0000

    Arrange words in file ending with '|'

--- node-core-examples/main.js
+++ node-core-examples/main.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
 
 let inputData = fs.readFileSync('mini-input.dat', 'utf-8');
-//console.log(file);
 let splitWords = inputData.split('|');
-//console.log(splitWords);
 
+let result = '';
 for ( i=0; i<splitWords.length-1; i++) {
     let currentWord = splitWords[i];
-    //console.log(firstWord);
-    let changedWord = currentWord[0].toUpperCase() + currentWord.slice(1);
-    console.log(changedWord);
+    var changedWord = currentWord[0].toUpperCase() + currentWord.slice(1);
+    result = result + changedWord + '|';
 }
+
+console.log(result);
